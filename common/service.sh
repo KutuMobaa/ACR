894a0d6591084ea57af9f7af0ffa9cd4f3dedd69
KutuMobaa <124151847+KutuMobaa@users.noreply.github.com>
2024-11-28 17:14:04 +0700

    Create service.sh

---
+++ common/service.sh
@@ -0,0 +1,14 @@
+MODDIR=${0%/*}
+
+while [[ -z $(resetprop sys.boot_completed) ]]; do sleep 5; done
+
+clear
+
+sleep 5
+$MODDIR/ACR 2>/dev/null
+
+# download module melalui channel atau grub telegram official Kutu Moba
+
+# https://t.me/kutuMobaUpdate
+
+# https://t.me/kutu_Moba57
